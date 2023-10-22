f12d8aefb1713bec69db29b89d761edbccbb3daf
Matsuuu <huhta.matias@gmail.com>
2023-10-22 17:36:54 +0300

    Base

---
+++ projects/index.js
@@ -0,0 +1,164 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO(yarr): Add some guideliness
+// TODO(lazy-dev): Check that the sum function only takes numbers as params
+
+function sum(a, b) {
+    return a + b;
+}
+
+const foo = 3;
+const bar = 5;
+
+const total = sum(foo, bar);
+
+// -------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------
+
+// TODO(boss-person): Enforce types of user props
+// TODO(mr-archi): Create an interface for user props
+// TODO(kid): Make sure phone is optional
+
+
+class User {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.phone = props.phone;
+    }
+}
+
+const userProps = {
+    name: "Matsu",
+    age: 27
+}
+
+const user = new User(userProps);
+
+// -------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------
+
+
+// TODO(the-p-o): Make sure padleft is usable using a string or a number
+// TODO(i-write-DRY-code): Create a re-usable number checker that does the narrowing thing for us
+
+function padLeft(padding, input) {
+    return padding + input;
+}
+
+const toPad = "foo";
+
+console.log(toPad);
+console.log(padLeft(5, toPad));
+console.log(padLeft("@@@@@", toPad));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
